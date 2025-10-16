eb12a03cc2435a8ffc041a4f713f005d7c70a687
zihang <yezihang@idea.edu.cn>
2025-10-16 15:52:47 +0800

    chore: adjust dev config

--- deno/web.ts
+++ deno/web.ts
@@ -1,5 +1,3 @@
-// @ts-check
-
 import { useState } from 'https://esm.sh/preact/hooks';
 import { html, render } from 'https://esm.sh/htm/preact';
 import { BlobWriter, HttpReader, TextReader, ZipWriter } from 'https://unpkg.com/@zip.js/zip.js/index.js';
